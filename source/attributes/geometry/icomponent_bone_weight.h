436236ab2b175cca77562bf60e05da4f57badaa5
Mikalaj <nickolaib2004@gmail.com>
2014-05-12 22:22:53 +0300

    geometry class complete

--- source/attributes/geometry/icomponent_bone_weight.h
+++ source/attributes/geometry/icomponent_bone_weight.h
@@ -7,6 +7,7 @@
 
 PUNK_ENGINE_BEGIN
 namespace Attributes {
+	class IArmatureSchema;
 	class IBoneWeightComponent {
 	public:
 		virtual const Math::vec4* GetWeights(std::uint32_t index) const = 0;
@@ -14,6 +15,8 @@ namespace Attributes {
 		virtual void SetVertexBonesWeight(std::uint32_t index, const Math::vec4& weights, const Math::ivec4& bones) = 0;
 		virtual void SetVertexBonesWeights(const Math::vec4v& weights, const Math::ivec4v& bones) = 0;
 		virtual bool HasVertexBoneWeights() const = 0;
+		virtual const IArmatureSchema* GetArmatureSchema() const = 0;
+		virtual void SetArmatureSchema(IArmatureSchema* value) = 0;
 	};
 }
 PUNK_ENGINE_END
