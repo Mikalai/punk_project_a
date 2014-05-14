bdf681006cb1e3e0398efd979b405f5fbbfd108b
Mikalaj <nickolaib2004@gmail.com>
2014-05-15 00:33:21 +0300

    object -> iobject

--- source/ai/curvepath.h
+++ source/ai/curvepath.h
@@ -2,14 +2,14 @@
 #define PUNK_AI_CURVEPATH_H
 
 #include <config.h>
-#include <core/object.h>
+#include <core/iobject.h>
 #include <math/spline.h>
 #include <math/curve.h>
 
 PUNK_ENGINE_BEGIN
 namespace AI
 {
-    class PUNK_ENGINE_API CurvePath : public Math::Curve, public Core::Object
+    class PUNK_ENGINE_API CurvePath : public Math::Curve, public Core::IObject
     {
     public:
         CurvePath();
@@ -22,7 +22,7 @@ namespace AI
     private:
         Core::String m_name;
     private:
-        PUNK_OBJECT(CurvePath)
+        PUNK_OBJECT_DEFAULT_IMPL(CurvePath)
     };
 }
 PUNK_ENGINE_END
